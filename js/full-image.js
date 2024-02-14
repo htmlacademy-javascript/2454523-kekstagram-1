16b0a106d3b7d6269ac34c65e38c77249521c238
Maria-Samoshkina <maria.edelshtein@gmail.com>
2024-02-14 13:30:55 +0200

    homework 8.15

--- js/full-image.js
+++ js/full-image.js
@@ -12,6 +12,7 @@ const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     bigPicture.classList.add('hidden');
+    document.body.classList.remove('modal-open');
   }
 };
 
@@ -24,21 +25,51 @@ const createCommentElement = (comment)=>{
   return commentElement;
 };
 
-const renderComments = (comments) => {
-  commentsContainerElement.innerHTML = '';
+const getSubCommentsArray = (firsIndex,lastIndex, comments) => comments.slice(firsIndex,lastIndex);
+
+const appendCommentsBlock = (comments) => {
   const commentsContainerFragment = document.createDocumentFragment();
   comments.forEach((comment) => {
     const commentElement = createCommentElement(comment);
     commentsContainerFragment.append(commentElement);
   });
   commentsContainerElement.append(commentsContainerFragment);
+};
+
+const renderComments = (comments) => {
+  commentsContainerElement.innerHTML = '';
+
+  let firsCommentIndex = 0;
+  let lastCommentsIndex = 5;
+
+  const currentCommentsArray = getSubCommentsArray(firsCommentIndex,lastCommentsIndex,comments);
+  appendCommentsBlock(currentCommentsArray);
+  let commentsCountForShow = currentCommentsArray.length;
+  bigPicture.querySelector('.social__comment-count').firstChild.textContent = `${commentsCountForShow } из `;
+
+  bigPicture.querySelector('.comments-loader').addEventListener('click', ()=> {
+    firsCommentIndex += 5;
+    lastCommentsIndex += 5;
+    if (lastCommentsIndex <= comments.length) {
+      const nextCommentsArray = getSubCommentsArray(firsCommentIndex,lastCommentsIndex,comments);
+      appendCommentsBlock(nextCommentsArray);
+      commentsCountForShow += 5;
+      bigPicture.querySelector('.social__comment-count').firstChild.textContent = `${commentsCountForShow } из `;
+    } else {
+      lastCommentsIndex = comments.length + 1;
+      const nextCommentsArray = getSubCommentsArray(firsCommentIndex,lastCommentsIndex,comments);
+      appendCommentsBlock(nextCommentsArray);
+      commentsCountForShow = comments.length;
+      bigPicture.querySelector('.social__comment-count').firstChild.textContent = `${commentsCountForShow } из `;
+    }
+  });
 
 };
 
 const openBigPicture = () => {
   bigPicture.classList.remove('hidden');
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+  bigPicture.querySelector('.social__comment-count').classList.remove('hidden');
+  bigPicture.querySelector('.comments-loader').classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   document.addEventListener('keydown', onDocumentKeydown);
@@ -74,4 +105,3 @@ const closeBigPicture = () => {
 bigPictureCloseElement.addEventListener('click', () => {
   closeBigPicture();
 });
-
