3beaabb5855270a09359d5ff95e1703d2bfece2a
Maria-Samoshkina <maria.edelshtein@gmail.com>
2024-04-08 23:58:44 +0300

    final code review fixes2

--- js/user-form.js
+++ js/user-form.js
@@ -24,6 +24,7 @@ const onDocumentKeydown = (evt) => {
       closeResultElement(errorElement);
     } else if(document.activeElement !== inputHashTag && document.activeElement !== inputTextDescription){
       evt.preventDefault();
+
       closeUserForm();
     }
   }
